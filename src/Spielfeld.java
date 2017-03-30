ee9b289231934af266bd63d08a688ad3366ff8f8
mariusglaess23 <mango.g@gmx.de>
2017-03-30 13:47:04 +0200

    unnötig

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -35,7 +35,25 @@ public class Spielfeld extends JFrame{
             JPanel enemyPane = new JPanel();
             enemyPane.setLayout(new GridLayout(rows, cols));
             for(int i = 0; i < AnzFelder; i++) {
+                final int h = i;
                 JButton button = new JButton(Integer.toString(i));
+                button.setForeground(new Color(255,255,255,0));
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("Button nummer: " + h);
+                        int t = Integer.parseInt(button.getText());
+                        if(t == 899){
+                            button.setBackground(Color.magenta);
+                            System.out.println("FUCK YEAH");
+                        }
+                        else{
+                            button.setBackground(Color.blue);
+                        }
+                    }
+                });
+                button.setPreferredSize(new Dimension(50,20));
+                button.setMinimumSize(new Dimension(50, 9));
                 enemyPane.add(button);
             }
             upper.add(enemyPane);
@@ -47,6 +65,8 @@ public class Spielfeld extends JFrame{
             ownPane.setLayout(new GridLayout(rows, cols));
             for(int j = 0; j < AnzFelder; j++){
                 JButton button2 = new JButton(Integer.toString(j));
+                button2.setPreferredSize(new Dimension(50,20));
+                button2.setMinimumSize(new Dimension(50, 9));
                 ownPane.add(button2);
             }
             lower.add(ownPane);
@@ -57,9 +77,9 @@ public class Spielfeld extends JFrame{
 
 
         myFrame.add(upper);
-        //myFrame.add(Box.createGlue());
-        myFrame.add(Box.createVerticalStrut(50));
-        //myFrame.add(lower);
+        myFrame.add(Box.createGlue());
+        //myFrame.add(Box.createVerticalStrut(50));
+        myFrame.add(lower);
         myFrame.pack();
         myFrame.setVisible(true);
     }
